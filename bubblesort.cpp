77db716ad16d741c45c2fea369808ba6846e6d66
Vaishnavi Ravindra Deshmukh <89265515+Unstopable18@users.noreply.github.com>
2022-03-17 17:15:26 +0530

    Add files via upload

---
+++ bubblesort.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+using namespace std;
+void swapping(int &a, int &b) 
+{      //swap the content of a and b
+   int temp;
+   temp = a;
+   a = b;
+   b = temp;
+}
+void display(int *array, int size) 
+{
+   for(int i = 0; i<size; i++)
+      cout << array[i] << " ";
+   cout << endl;
+}
+void bubbleSort(int *array, int size) 
+{
+   for(int i = 0; i<size; i++) 
+   {
+      int swaps = 0;         
+      for(int j = 0; j<size-i-1; j++) 
+      {
+         if(array[j] > array[j+1]) 
+         {       
+            swapping(array[j], array[j+1]);
+            swaps = 1;    
+         }
+      }
+      if(!swaps)
+         break;       
+   }
+}
+int main() 
+{
+   int n;
+   cout<<"\n*************************************\n";
+   cout << "Enter The Total Number of Array Elements: ";
+   cin >> n;
+   int arr[n];     
+   cout << "\nEnter Array Elements:" << endl;
+   for(int i = 0; i<n; i++) 
+   {
+    cin >> arr[i];
+   }
+   cout<<"\n*************************************\n";
+   cout << " Unsorted Array: ";
+   display(arr, n);
+   bubbleSort(arr, n);
+   cout<<"\n*************************************\n";
+   cout << " Bubble Sorted Array: ";
+   display(arr, n);
+   cout<<"\n*************************************\n";
+}
